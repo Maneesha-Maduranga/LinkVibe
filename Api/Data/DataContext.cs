e41208f25e85316bab278afbfdcc42ab45c9931f
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-12-13 20:42:41 +0530

    Add DataContext class

---
+++ Api/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Api.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
