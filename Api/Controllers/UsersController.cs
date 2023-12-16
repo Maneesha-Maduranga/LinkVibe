f4c46b8a9a8f4f7c20d104427a496a4fee09e930
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-12-16 13:50:14 +0530

    Add GetUsers method in UsersController

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Api.Data;
+using Api.Entites;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return Ok(users);
+            
+        }
+    }
+}
